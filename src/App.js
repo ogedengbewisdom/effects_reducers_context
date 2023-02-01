ab73461185f0a9a0d26b81c3394afdc1fbbe4b9a
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-01 18:35:18 +0100

    Tapping into context with useContext Hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useContext } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,35 +6,17 @@ import MainHeader from './components/MainHeader/MainHeader';
 import AuthContext from './components/Store/auth_context';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-useEffect(() => {
-  const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn")
-  if (storedUserLoggedInInformation) {
-    setIsLoggedIn(true)
-  }
-}, [])
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem("isLoggedIn", "1")
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem("isLoggedIn")
-    setIsLoggedIn(false);
-  };
+  const ctx = useContext(AuthContext)
 
   return (
-    <AuthContext.Provider value={{isLoggedIn: isLoggedIn, onLogout: logoutHandler}}>
+    <Fragment>
       <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
-    </AuthContext.Provider >
+    </Fragment >
   );
 }
 
