59a409eb28e85ca78c6a50a03e07c9bd97e627e4
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-02 13:31:07 +0100

    Reusable input component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,8 +1,19 @@
 
-import React from "react";
+import React, { useRef, useImperativeHandle } from "react";
 import classes from "./Input.module.css"
 
-const Input = (props) => {
+const Input = React.forwardRef((props,ref) => {
+    const inputRef = useRef()
+
+    const activate = () => {
+        inputRef.current.focus()
+    }
+
+    useImperativeHandle(ref,() => {
+        return {
+            focus: activate
+        }
+    })
 
     return (
             <div
@@ -12,6 +23,7 @@ const Input = (props) => {
             >
                 <label htmlFor={props.id}>{props.label}</label>
                 <input
+                ref={inputRef}
                 type={props.type}
                 id={props.id}
                 value={props.value}
@@ -21,5 +33,5 @@ const Input = (props) => {
             </div>
             )
 }
-
+)
 export default Input
